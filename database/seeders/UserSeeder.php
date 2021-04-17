96b3aa5603a6319f5165e0743a928f35b2e478f4
Kiran <khadkakiran18@gmail.com>
2021-04-17 11:08:32 +0545

    User Role and Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,5 +14,11 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
+            'role' => 'Admin',
+        ]);
     }
 }
