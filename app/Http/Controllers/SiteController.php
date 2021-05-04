3fab7b1b1440a73bd1e2fecc2c6d11cc23df7514
Kiran <khadkakiran18@gmail.com>
2021-05-04 11:51:43 +0545

    Admin Middleware

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function home()
+    {
+        if (auth()->user()->role == "Admin") {
+            return redirect('/admin');
+        }
+        return redirect('/');
+    }
 }
